4db7cb232b69836480c6a17569380e5a59107bb2
Rongjian Zhang <pd4d10@gmail.com>
2020-01-27 12:33:46 +0800

    feat: token api

---
+++ api/token.js
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+
+export default async (req, res) => {
+  const r = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      ...req.body,
+      client_secret: process.env.CLIENT_SECRET
+    })
+  });
+  const json = await r.json();
+  res.json(json);
+};
